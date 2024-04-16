96d08f97ce39c0ac4f63b934f734193c752013dd
sSsDarksSs <166707046+sSsDarksSs@users.noreply.github.com>
2024-04-16 16:16:46 +0700

    file

---
+++ CuoiKyNodejs/server/models/Admin.js
@@ -0,0 +1,9 @@
+import mongoose, { Types } from "mongoose";
+
+const adminSchema = new mongoose.Schema({
+    username: {type: String, required:true, unique: true},
+    password: {type: String, required:true}
+})
+
+const adminModel = mongoose.model('Admin', adminSchema)
+export {adminModel as Admin}
